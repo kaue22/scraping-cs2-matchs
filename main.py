1aff0d6564447ca42394000b6ee69050ab009721
Kaue Lima <kauegouveia@gmail.com>
2024-09-15 09:31:29 -0300

    feat:Removendo comentários

--- main.py
+++ main.py
@@ -8,25 +8,19 @@ locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 def extract_news_titles():
     formatter = "%d de %B, %Y"
     url = "https://www.dust2.com.br/resultados"
-    
-    # Faz a requisição para a URL e obtém o HTML da página
+
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Seleciona os cabeçalhos do grupo de partidas
     matches_group_headers = soup.select(".matches-group-header")
     
     for header in matches_group_headers:
-        # Converte o texto do cabeçalho para uma data
         received_date = datetime.strptime(header.text.strip(), formatter).date()
         if received_date == (datetime.now().date() - timedelta(days=1)):
-            # Seleciona os containers de resultados de partidas
             matches_group_containers = soup.select(".match-results")
             
             for container in matches_group_containers:
                 for link in container.select("a[href^='/partidas/']"):
                     href = link['href']
                     print(href)
-
-# Executa a função
 extract_news_titles()
